6326fcb36590ac3746d4948bd5ee4d19ee6ebd49
kodonghee <qmdlfakrhf@naver.com>
2025-05-28 22:13:32 +0900

    Create Calculator.java
    Calculator 클래스 생성
    calc 객체 생성
    사칙연산 메소드 생성 및 호출

---
+++ Java/자바의 신/Chapter03/Calculator.java
@@ -0,0 +1,33 @@
+public class Calculator {
+
+    public static void main(String[] args){
+        System.out.println("Calculator class started");
+        Calculator calc = new Calculator();
+        int a = 10;
+        int b = 5;
+        System.out.println("add = " + calc.add(a, b));
+        System.out.println("substract = " + calc.subtract(a, b));
+        System.out.println("multiply = " + calc.multiply(a, b));
+        System.out.println("divide = " + calc.divide(a, b));
+    }
+
+    public int add(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public int subtract(int num1, int num2){
+        int diff = num1 - num2;
+        return diff;
+    }
+
+    public int multiply(int num1, int num2){
+        int result = num1 * num2;
+        return result;
+    }
+
+    public int divide(int num1, int num2){
+        int result = num1 / num2;
+        return result;
+    }
+}
